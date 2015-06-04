c84b3e4ebf2503ee1af5292c6c90c8075e30afc3
ANightCoder-Brian <ANightCoder@comcast.net>
2015-06-04 12:44:37 -0400

    Adding various 'untracked' files...

---
+++ HISWebClient/Scripts/Custom/TimeSeriesRequestStatus.js
@@ -0,0 +1,30 @@
+﻿//
+//A JavaScript 'enum' to time series request status values
+//NOTE: Values defined here and in the C# equivalent MUST always match!!
+
+//Implementation - Dynamic Prototype Pattern...
+
+// Usage:   var timeSeriesRequestStatus = new TimeSeriesRequestStatus();
+//          timeSeriesRequestStatus.Completed - 'enum' value - 5
+//          timeSeriesRequestStatus.properties[timeSeriesRequestStatus.Completed].description - 'enum' description - 'Completed!!'
+
+function TimeSeriesRequestStatus() {
+
+    //Enum values...
+    this.enumValues = { "Starting": [1, "Starting..."],
+                        "Started": [2, "Started!!"],
+                        "ClientSubmittedCancelRequest": [3, "Client submitted cancellation request..."],
+                        "CanceledPerClientRequest": [4, "Canceled per client request!!"],
+                        "Completed": [5, "Completed!!"],
+                        "ProcessingTimeSeriesId": [6, "Processing Time Series ID: "],
+                        "SavingZipArchive": [7, "Saving Zip archive..."],
+                        "ProcessingError": [8, "Processing error: "],
+                        "UnknownTask": [9, "Unknown Task!!"]
+                      };
+
+    if ("function" !== typeof this.getEnum) {
+        TimeSeriesRequestStatus.prototype.getEnum = function () {
+            return (freezeEnum(this.enumValues));
+        }
+    }
+}
