f21fb5e534994ccee75776e1dd3ffe75a9d7e708
ANightCoder-Brian <ANightCoder@comcast.net>
2015-06-22 20:21:41 -0400

    Initialized current place name (United States) to match initial latitude and longitude (39, -92) for the map...

--- HISWebClient/Scripts/Custom/webclient.js
+++ HISWebClient/Scripts/Custom/webclient.js
@@ -40,7 +40,10 @@ function initialize() {
 
     var myCenter = new google.maps.LatLng(39, -92); //us
     //var myCenter = new google.maps.LatLng(42.3, -71);//boston
-   // var myCenter = new google.maps.LatLng(41.7, -111.9);//Salt Lake
+    // var myCenter = new google.maps.LatLng(41.7, -111.9);//Salt Lake
+
+    //NOTE: Set the place name to match the initial latitude and longitude 
+    currentPlaceName = 'United States';
     
       //BC - disable infoWindow for now...
 //    infoWindow = new google.maps.InfoWindow();
