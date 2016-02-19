38b13a3769f605b06ca04f2bf4df2248b494b355
ANightCoder-Brian <ANightCoder@comcast.net>
2016-02-19 14:50:07 -0500

    BCC - 19-Feb-2016 - CUAHSI HydroClient Release 1.2 - New Code Files

---
+++ HISWebClient/Models/ClientFilterAndSearchCriteria/clientFilterAndSearchCriteria.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HISWebClient.Models.ClientFilterAndSearchCriteria
+{
+	public class clientFilterAndSearchCriteria
+	{
+		public clientFilterAndSearchCriteria()
+		{
+			dataSources = new List<clientDataSource>();
+
+			filters = new List<clientFilter>();
+		}
+
+		public List<clientDataSource> dataSources { get; set; }
+
+		public List<clientFilter> filters { get; set; }
+
+		public string search { get; set; }
+
+		public bool isEmpty()
+		{
+			return ( String.IsNullOrWhiteSpace(search) && (0 >= filters.Count));
+		}
+	}
+}
