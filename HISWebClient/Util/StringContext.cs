b1910ba551350730594c7b3ed8add3630bf7e394
ANightCoder-Brian <ANightCoder@comcast.net>
2016-02-19 14:54:45 -0500

    BCC - 19-Feb-2016 - CUAHSI HydroClient Release 1.2 - New code files...

---
+++ HISWebClient/Util/StringContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HISWebClient.Util
+{
+	//Utility methods for strings...
+	public static class StringContext
+	{
+
+		//Extension Contains method which accepts a StringComparison parameter...
+		public static bool Contains(this string stringLHS, string stringRHS, StringComparison stringComparison)
+		{
+			//Validate/initialize input parameters...
+			// NOTE: StringComparison parameter can never be null!!
+			if ( null == stringLHS || null == stringRHS || //null == stringComparison ||
+				 String.IsNullOrWhiteSpace(stringLHS) ||
+				 String.IsNullOrWhiteSpace(stringRHS))
+			{
+				return false;
+			}
+
+			return stringLHS.IndexOf(stringRHS, stringComparison) >= 0;
+		}
+
+	}
+}
